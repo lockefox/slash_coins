8fdb18dbc46c28fef7534c1a2a56a40db1c2118d
Lockefox <locke.renard@gmail.com>
2017-12-20 15:27:54 -0800

    adding log builder for pythonanywhere

--- slash_coins/flask_launcher.py
+++ slash_coins/flask_launcher.py
@@ -7,7 +7,7 @@ from flask import Flask
 from flask_restful import Api
 
 import prosper.common.prosper_cli as p_cli
-import prosper.common.prosper_logging as prosper_logging
+import prosper.common.prosper_logging as p_logging
 import prosper.common.prosper_config as p_config
 
 from . import _version
@@ -20,6 +20,36 @@ API = Api(APP)
 API.add_resource(endpoints.Root, '/')
 API.add_resource(endpoints.Version, '/version')
 API.add_resource(endpoints.CoinQuote, '/coins')
+
+def build_logger_pythonanywhere(
+        config_path,
+        log_name=_version.PROGNAME,
+        debug=False
+):
+    """build a logger outside the CLI FlaskLauncher
+
+    Args:
+        config_path (str): path to config file
+        log_name (str): name of log
+        debug (bool): enable stdout loggers and disable webhook loggers
+
+    """
+    config = p_config.ProsperConfig(config_path)
+    builder = p_logging.ProsperLogger(
+        log_name,
+        config.get('LOGGING', 'log_path'),
+        config_obj=config
+    )
+
+    if debug:
+        builder.configure_debug_logger()
+    else:
+        if config.get('LOGGING', 'discord_webhook'):
+            builder.configure_discord_logger()
+        if config.get('LOGGING', 'slack_webhook'):
+            builder.configure_slack_logger()
+        if config.get('LOGGING', 'hipchat_webhook'):
+            builder.configure_hipchat_logger()
 
 class FlaskLauncher(p_cli.ProsperApplication):
     PROGNAME = _version.PROGNAME
