80ff379d93a843a3c1c54a0a13ec48cdaf8322f6
Lockefox <locke.renard@gmail.com>
2017-12-26 14:17:18 -0800

    fixing __version__ in docs

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./slash_coins'))
+sys.path.insert(0, os.path.abspath('../slash_coins'))
 import alabaster
 
 from slash_coins._version import __version__
